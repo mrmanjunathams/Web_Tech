07f0bdb8d9a1574f56e606ac86161228dfe7ed7c
mrmanjunathams <mrmanjunathams@gmail.com>
2018-11-09 10:57:58 +0530

    final

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ var app =angular.module("myApp",["ngRoute"]);
 
 app.config(function($routeProvider){
     $routeProvider.when("/",{
-      templateUrl:"templets/home.html"
+      templateUrl:"templets/home2.html"
     }).when("/blog",{
         templateUrl:"templets/blog.html"
     }).when("/aboutme",{
