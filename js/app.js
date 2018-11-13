b60843696a9ab2b06f7d986cacd0dffcd98bca67
mrmanjunathams <mrmanjunathams@gmail.com>
2018-11-13 22:45:59 +0530

    v1.0

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ var app =angular.module("myApp",["ngRoute"]);
 
 app.config(function($routeProvider){
     $routeProvider.when("/",{
-      templateUrl:"templets/home2.html"
+      templateUrl:"templets/home.html"
     }).when("/blog",{
         controller:"blogscontroller",
         templateUrl:"templets/blog.html"
